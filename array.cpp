1c4ba826413de4d9a923c27d24117062d16a3967
bagasadit123 <bagasadit430@gmail.com>
2026-04-06 17:50:18 +0700

    finish

--- array.cpp
+++ array.cpp
@@ -36,4 +36,9 @@ int main() {
                 cin >> kamu[i];
           }
             cout << endl;
+             // Menampilkan dengan menggunakan perulangan for
+          for (int i = 0; i <= 4; i++)
+          {
+                cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+          }
 }
